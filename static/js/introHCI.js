fa28d27fbc93fea4455b18467eee175dd071e225
yuerongX <y3xiao@ucsd.edu>
2021-01-21 01:17:17 -0800

    done lab3

--- static/js/introHCI.js
+++ static/js/introHCI.js
@@ -3,6 +3,7 @@
 // Call this function when the page loads (the "ready" event)
 $(document).ready(function() {
 	initializePage();
+	projectClick();
 })
 
 /*
@@ -10,9 +11,25 @@ $(document).ready(function() {
  */
 function initializePage() {
 	$("#testjs").click(function(e) {
-		$('.jumbotron h1').text("Javascript is connected");
+		$('.jumbotron h1').text("Javascript has taken control by yuerong");
+		$(".jumbotron p").toggleClass("active");
+		$("#testjs").text("Please wait...");
+		
 	});
 
 	// Add any additional listeners here
 	// example: $("#div-id").click(functionToCall);
+	$("a.thumbnail").click(projectClick);
+}
+
+function projectClick(e){
+	e.preventDefault();
+	var containingProject = $(this).closest(".project");
+    var description = $(containingProject).find(".project-description");
+    if (description.length == 0) {
+       $(containingProject).append("<div class='project-description'><p>Description of the project.</p></div>");
+    } else {
+		$(description).hide(".project-description");
+		description.length = 0;
+    }
 }
